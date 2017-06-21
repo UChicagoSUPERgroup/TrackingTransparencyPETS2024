871e074a9ca8255fe6223f6ea7229001de5a1bc1
Ben Weinshel <weinshel@uchicago.edu>
2017-06-21 14:56:26 -0500

    use async/await

--- popup/popup.js
+++ popup/popup.js
@@ -1,13 +1,13 @@
-$('document').ready(function(){
-  browser.tabs.query({active: true, lastFocusedWindow: true})
-  .then((tabs) => {
-    let title = tabs[0].title;
-    if (title.length >= 30) {
-      title = title.substring(0,30).concat("...");
-    }
-    $('#pagetitle').text(title);
-  });
-});
+async function onReady() {
+  let tabs = await browser.tabs.query({active: true, lastFocusedWindow: true})
+  let title = tabs[0].title;
+  if (title.length >= 30) {
+    title = title.substring(0,30).concat("...");
+  }
+  $('#tt_hook_origin_title').text(title);
+}
+
+$('document').ready(onReady());
 
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("show-more-btn")) {
