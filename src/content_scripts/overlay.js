79dc79897260909566a4a88e3e611308a226847c
Ben Weinshel <weinshel@uchicago.edu>
2017-08-15 16:55:15 -0500

    working overlay

--- src/content_scripts/overlay.js
+++ src/content_scripts/overlay.js
@@ -1,14 +1,21 @@
 /* OVERLAY */
 
-$(document).ready(function() {
-  var overlay = Boundary.createBox("overlay");
-  Boundary.loadBoxCSS("#overlay", browser.extension.getURL('stylesheets/overlay-inner.css'));
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-  Boundary.rewriteBox("#overlay", "<div id='message'>Hi there!</div>");
+async function onReady() {
+  var overlay = Boundary.createBox("tt_overlay_64f9de");
+  await sleep(100);
+  Boundary.loadBoxCSS("#tt_overlay_64f9de", browser.extension.getURL('stylesheets/overlay-inner.css'));
+
+  Boundary.rewriteBox("#tt_overlay_64f9de", "<div id='message'>Hi there!</div>");
 
   // Boundary.appendToBox("#overlay", "<div><button class='button'>Click me first!</button></div>");
 	// /* add some silly interaction to box one */
 	// Boundary.findElemInBox(".button", "#overlay").click(function() {
 	// 	Boundary.appendToElemInBox("#message", "#yourBoxOneID", "<br>Now click on the second button.");
   // });
-});
+}
+
+$(document).ready(onReady());
