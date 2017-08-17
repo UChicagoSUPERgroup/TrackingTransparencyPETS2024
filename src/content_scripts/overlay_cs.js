fcb5cefc8b0397c1a5b2956c0fd0830807fb1f46
Ben Weinshel <weinshel@uchicago.edu>
2017-08-17 10:50:24 -0500

    overlay improvements

--- src/content_scripts/overlay_cs.js
+++ src/content_scripts/overlay_cs.js
@@ -4,16 +4,14 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-var overlay = document.createElement("div");
-var p = document.createElement("p");
-overlay.appendChild(p);
-overlay.id = "tt_overlay_64f9de";
+async function injectOverlay() {
+  var overlay = document.createElement("div");
+  // var p = document.createElement("p");
+  // overlay.appendChild(p);
+  overlay.id = "tt_overlay_64f9de";
+  overlay.innerHTML = '<div style="font: 10pt sans-serif; padding: 2px;">Tracking Transparency is tracking the trackers!</div>';
 
-var line1 = document.createTextNode("3 third-parties");
-var line2 = document.createTextNode("25 pages");
+  document.body.appendChild(overlay);
+}
 
-p.appendChild(line1);
-p.appendChild(document.createElement('br'));
-p.appendChild(line2);
-
-document.body.appendChild(overlay);
+injectOverlay();
