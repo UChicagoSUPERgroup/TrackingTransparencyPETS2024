15714c743e6dba386ab9733b205a3f8ef4310166
Aaron <argold@uchicago.edu>
2017-08-20 20:45:30 -0500

    FAQ

--- extension/infopage/infopage.js
+++ extension/infopage/infopage.js
@@ -103,6 +103,27 @@ document.addEventListener("click", (e) => {
 });
 
 
+// FAQ-Accordion Animation
+$(document).ready(function() {
+
+  var acc = document.getElementsByClassName("faq-accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight){
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    }
+  }
+});
+
+
+
 
 //we need to 'clean' tracker names before we use them inside html ids because
 // trackers can have periods or spaces in their names which mess things up
