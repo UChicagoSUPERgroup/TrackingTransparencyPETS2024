6f9a9b58f81a6957acbeb8ff4b993bcd83f07cbc
Ben Weinshel <weinshel@uchicago.edu>
2017-08-28 19:39:23 -0500

    working treemap!

--- src/database/queries.js
+++ src/database/queries.js
@@ -386,6 +386,33 @@ async function getInfoAboutTracker(tracker, inferenceCount, pageCount) {
   return inferenceInfo;
 }
 
+
+/**
+ * Count of how many times an inference has been made
+ * 
+ * TODO: right now we use this on infopage and make a query for every single possible inferce
+ * when it would be better just to have one query that returns counts of all inferences
+ *
+ * @param {string} inference
+ * @returns {string[]} array of trackers
+ */
+async function getInferenceCount(inference) {
+  let query = await ttDb.select(lf.fn.count(Inferences.inference))
+    .from(Inferences)
+    .where(Inferences.inference.eq(inference))
+    .groupBy(Inferences.inference)
+    .exec();
+  let res;
+  if (typeof query != "undefined" && query != null && query.length > 0) {
+    res = (query[0])['COUNT(inference)'];
+  } else {
+    res = "0";
+  }
+  // console.log(res);
+  return res;
+}
+
+
 /* ========= */
 
 /**
@@ -451,6 +478,8 @@ export default async function makeQuery(query, args) {
     case "get_domains_no_trackers":
       res = await getDomainsNoTrackers();
       break;
+    case "get_inference_count":
+      res = await getInferenceCount(args.inference);
   }
 
   return res;
