eed8ff35c58163f18b166969287a3b9d677d6edf
Ben Weinshel <weinshel@uchicago.edu>
2017-12-14 15:38:39 -0600

    clean up config files

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     background: './src/background_scripts/background.js',
     userstudy: './src/background_scripts/userstudy.js',
 
-    // workers
+    // web workers for background script
     trackers_worker: './src/trackers/trackers.worker.js',
     inferencing_worker: './src/inferencing/inferencing.worker.js',
     database_worker: './src/database/database.worker.js',
@@ -18,10 +18,11 @@ module.exports = {
     // content scripts
     content: './src/content_scripts/content.js',
 
+    // user facing pages
     popup: './src/popup/popup.js',
     dashboard: './src/dashboard/index.js',
-    infopage: './src/infopage/infopage.js',
-    debugscreen: './src/debugscreen/debugscreen.js'
+    infopage: './src/infopage/infopage.js', // DEPRECATED
+    debugscreen: './src/debugscreen/debugscreen.js' // DEPRECATED
 
   },
   output: {
@@ -34,14 +35,23 @@ module.exports = {
   module: {
     rules: [
       { 
-        test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" 
+        // Babel transpilation
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: "babel-loader",
+        options: {
+          "presets": ["env", "react"],
+          "plugins": ["transform-eval", "transform-runtime", "transform-object-rest-spread"]
+        }
       },
       {
+        // properly load font files
         // https://github.com/webpack-contrib/css-loader/issues/38#issuecomment-313673931
         test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
         loader: 'file-loader?name=fonts/[name].[ext]'
       },
       {
+        // allow importing css files
         test: /\.css$/,
         use: [
           { loader: "style-loader" },
@@ -74,6 +84,8 @@ module.exports = {
   // This will expose source map files so that errors will point to your
   // original source files instead of the transpiled files.
   devtool: 'inline-source-map',
+
+  // fix importing some dependencies that assume filesystem etc.
   node: {
     fs: "empty",
     net: "empty",
