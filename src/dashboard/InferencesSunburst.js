62e3b8d367087ef37db9f5e21b6572d98ca4ca60
Ben Weinshel <weinshel@uchicago.edu>
2017-12-14 01:20:11 -0600

    it appears duplication hack is not needed

--- src/dashboard/InferencesSunburst.js
+++ src/dashboard/InferencesSunburst.js
@@ -2,9 +2,7 @@ import React from 'react';
 import {Sunburst, LabelSeries} from 'react-vis';
 import ttDashboard from './dashboardHelpers';
 
-import D3FlareData from './data/d3-flare-example.json';
-
-import categoryTree from './data/categories.json';
+import categoryTree from './data/categories_no_duplicate.json';
 
 const EXTENDED_DISCRETE_COLOR_RANGE = ['#19CDD7', '#DDB27C', '#88572C', '#FF991F', '#F15C17', '#223F9A', '#DA70BF', '#125C77', '#4DC19C', '#776E57', '#12939A', '#17B8BE', '#F6D18A', '#B7885E', '#FFCB99', '#F89570', '#829AE3', '#E79FD5', '#1E96BE', '#89DAC1', '#B3AD9E'];
 
@@ -52,7 +50,6 @@ function updateData(data, keyPath) {
   return data;
 }
 
-// const decoratedData = updateData(D3FlareData, false);
 
 export default class BasicSunburst extends React.Component {
   constructor(props) {
@@ -93,18 +90,15 @@ export default class BasicSunburst extends React.Component {
   recursiveApplySizes(root, inferencesList) {
     // let newChildren = [];
     for (let item of root.children) {
-      if (!item.children) {
-        // no children, lookup size
-        const listItem = inferencesList.find(x => x.inference === item.name);
-        if (listItem) {
-          item.size = listItem["COUNT(inference)"];
-        //   newChildren.push(item);
-        } else {
-          item.size = 0;
-        }
+      const listItem = inferencesList.find(x => x.inference === item.name);
+      if (listItem) {
+        item.size = listItem["COUNT(inference)"];
+      //   newChildren.push(item);
       } else {
+        item.size = 0;
+      }
+      if (item.children) {
         this.recursiveApplySizes(item, inferencesList);
-        // res.push(item);
       }
     }
 
