9b84e1e0e95a75c20a67c68fabce6ee89fadb4b1
Ben Weinshel <weinshel@uchicago.edu>
2017-12-22 00:45:59 -0600

    reorganize home page, add new summary

--- src/dashboard/App.js
+++ src/dashboard/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
-import {
-  HashRouter,
-  Route,
-  Link
-} from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 import Navbar from 'react-bootstrap/lib/Navbar';
 import Nav from 'react-bootstrap/lib/Nav';
@@ -15,6 +11,7 @@ import {LinkContainer} from 'react-router-bootstrap';
 import '../../node_modules/react-vis/dist/style.css';
 import 'typeface-source-sans-pro';
 
+import {Home, WaitingDataHome} from './Home';
 import InferencesPage from './Inferences';
 import TrackersList from './Trackers';
 import FirstPartyList from  './FirstParties';
@@ -24,6 +21,38 @@ import DebugPage from './Debug';
 import tt from '../helpers';
 import './App.css';
 
+const NavLink = ({to, title}) => (
+  <LinkContainer to={to}>
+    <NavItem>{title}</NavItem>
+  </LinkContainer>
+)
+
+const TTNavbar = () => {
+  const enoughData = tt.enoughData();
+  return (
+    <Navbar fixedTop>
+      <Navbar.Header>
+        <LinkContainer to="/">
+          <Navbar.Brand>Tracking Transparency</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        {enoughData && <Nav>
+          <NavLink to="/trackers" title="What have they tracked?"/>
+          <NavLink to="/inferences" title="What could they have learned?"/>
+          <NavLink to="/recent" title="Recent Activity"/>
+          <NavLink to="/domains" title="Domains"/>
+        </Nav>}
+        <Nav pullRight>
+          <NavLink to="/debug" title="Debug"/>
+          <NavLink to="/about" title="About"/>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
 class App extends Component {
   render() {
     const enoughData = tt.enoughData();
@@ -31,60 +60,17 @@ class App extends Component {
     return(
       <HashRouter>
         <div>
-          <Navbar fixedTop>
-            <Navbar.Header>
-              <LinkContainer to="/">
-                <Navbar.Brand>
-                  Tracking Transparency
-                </Navbar.Brand>
-              </LinkContainer>
-              <Navbar.Toggle />
-            </Navbar.Header>
-            <Navbar.Collapse>
-              {enoughData && <Nav>
-                <LinkContainer to="/trackers">
-                  <NavItem>
-                    What have they tracked?
-                  </NavItem>
-                </LinkContainer>
-                <LinkContainer to="/inferences">
-                  <NavItem>
-                    What could they have learned?
-                  </NavItem>
-                </LinkContainer>
-                <LinkContainer to="/recent">
-                  <NavItem>
-                    Recent Activity
-                  </NavItem>
-                </LinkContainer>
-                <LinkContainer to="/domains">
-                  <NavItem>
-                    Domains
-                  </NavItem>
-                </LinkContainer>
-              </Nav>}
-              <Nav pullRight>
-                <LinkContainer to="/debug">
-                  <NavItem>
-                    Debug
-                  </NavItem>
-                </LinkContainer>
-                <LinkContainer to="/about">
-                  <NavItem>
-                    About
-                  </NavItem>
-                </LinkContainer>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          
+          <TTNavbar/>
           
           <div className="container containerInner">
-            {enoughData &&<Route exact path="/" component={Home}/>}
-            {enoughData &&<Route path="/inferences" component={InferencesPage}/>}
-            {enoughData &&<Route path="/trackers" component={TrackersList}/>}
-            {enoughData &&<Route path="/domains" component={FirstPartyList}/>}
-            {enoughData &&<Route path="/recent" component={RecentPage}/>}
+
+            {enoughData && <div>
+              <Route exact path="/" component={Home}/>
+              <Route path="/inferences" component={InferencesPage}/>
+              <Route path="/trackers" component={TrackersList}/>
+              <Route path="/domains" component={FirstPartyList}/>
+              <Route path="/recent" component={RecentPage}/>
+            </div>}
 
             {!enoughData &&<Route exact path="/" component={WaitingDataHome}/>}
 
@@ -97,23 +83,5 @@ class App extends Component {
   }
 }
 
-const Home = () => (
-  <div>
-    <h1>Tracking Transparency</h1>
-    <div className="homeText">
-      <p>Learn about online tracking!</p>
-      <p>Short example here…</p>
-      <p>See all the the trackers and inferences on a specific domain, such as <Link to={{pathname: '/domains/www.nytimes.com'}}>www.nytimes.com</Link> or <Link to={{pathname: '/domains/www.yahoo.com'}}>www.yahoo.com</Link>. Learn about a specific tracker such as <Link to={{pathname: '/trackers/Google'}}>Google</Link>. See all the <Link to={{pathname: '/inferences'}}>inferences</Link>  companies may have made about your browsing, or view details about a specific inference such as <Link to={{pathname: '/inferences/warehousing'}}>warehousing</Link>.</p>
-    </div>
-  </div>
-)
 
-const WaitingDataHome = () => (
-  <div>
-    <h1>Tracking Transparency</h1>
-    <div className="homeText">
-      <p>Continue using the internet and come back here in a few days to see insights about what companies know about your browsing!</p>
-    </div>
-  </div>
-)
 export default App;
---
+++ src/dashboard/Home.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+export class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  async getData() {
+    const background = await browser.runtime.getBackgroundPage();
+
+    const numPages = background.queryDatabase('getNumberOfPages', {});
+    const numTrackers = background.queryDatabase('getNumberOfTrackers', {});
+    const numInferences = background.queryDatabase('getNumberOfInferences', {});
+
+    // we use promises here instead of async/await because queries are not dependent on each other
+    numPages.then(n => this.setState({numPages: n}));
+    numTrackers.then(n => this.setState({numTrackers: n}));
+    numInferences.then(n => this.setState({numInferences: n}));
+  }
+
+  async componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    const {numTrackers, numInferences, numPages} = this.state;
+    return (
+      <div>
+        <h1>Tracking Transparency</h1>
+        <div className="homeText">
+
+          <p>The Tracking Transparency extension lets you learn about what companies could have inferrred about your browsing through trackers and advertisments on the web pages you visit.</p>
+
+          <p>In total, <em>{numTrackers} trackers</em> have seen you visit <em>{numPages} pages</em>. The Tracking Transparency extension has determined that these companies could have inferred your interest in <em>{numInferences} topics</em>.</p>
+
+          {/* <p>See all the the trackers and inferences on a specific domain, such as <Link to={{pathname: '/domains/www.nytimes.com'}}>www.nytimes.com</Link> or <Link to={{pathname: '/domains/www.yahoo.com'}}>www.yahoo.com</Link>. Learn about a specific tracker such as <Link to={{pathname: '/trackers/Google'}}>Google</Link>. See all the <Link to={{pathname: '/inferences'}}>inferences</Link>  companies may have made about your browsing, or view details about a specific inference such as <Link to={{pathname: '/inferences/warehousing'}}>warehousing</Link>.</p> */}
+        </div>
+      </div>
+    )
+  }
+}
+
+export const WaitingDataHome = () => (
+  <div>
+    <h1>Tracking Transparency</h1>
+    <div className="homeText">
+      <p>Continue using the internet and come back here in a few days to see insights about what companies know about your browsing!</p>
+    </div>
+  </div>
+)
