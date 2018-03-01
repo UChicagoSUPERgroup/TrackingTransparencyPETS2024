56944ec14bd9adc97973684ddd9dbb1c45f8255d
Ben Weinshel <weinshel@uchicago.edu>
2018-03-01 12:24:21 -0600

    trying to get tests to work… …and not succeeding

--- test/test.js
+++ test/test.js
@@ -1,27 +1,23 @@
+const test = require('tape');
 const puppeteer = require('puppeteer');
 
 const CRX_PATH = '../extension/';
 
-puppeteer.launch({
-  headless: false, // extensions only supported in full chrome.
-  args: [
-    `--disable-extensions-except=${CRX_PATH}`,
-    `--load-extension=${CRX_PATH}`,
-    '--user-agent=PuppeteerAgent'
-  ]
-}).then(async browser => {
+async function runTests() {
+  const browser = await puppeteer.launch({
+    headless: false, // extensions only supported in full chrome.
+    args: [
+      `--disable-extensions-except=${CRX_PATH}`,
+      `--load-extension=${CRX_PATH}`,
+      '--user-agent=PuppeteerAgent'
+    ]
+  })
   
-  // a hack to get id of current extension by loading options page and finding where it is displayed in texted
-  const page = await browser.newPage();
-  await page.goto('chrome://extensions');
-  const devToggle = await page.click('#toggle-dev-on');
-  const idHandle = await (await page.$('.extension-id'));
-  let id = await page.evaluate(body => body.textContent, idHandle);
-  id = id.trim();
+  const id = await getExtensionId(browser);
   const BASE_URL = 'chrome-extension://' + id + '/';
 
-
   // visit some pages
+  const page = await browser.newPage();
   await page.goto('https://www.nytimes.com');
   await page.goto('https://super.cs.uchicago.edu');
   await page.goto('https://cs.uchicago.edu');
@@ -39,8 +35,13 @@ puppeteer.launch({
     let ping = window.ping();
     console.log(ping) // this works
 
-    let query = await window.queryDatabase('getAllData', {});
-    console.log(query) // this hangs? idk why
+    // test('ping test', function (t) {
+    //   let ping = window.ping();
+    //   t.equal(ping, 'ping');
+    // });
+
+    // let query = await window.queryDatabase('getAllData', {});
+    // console.log(query) // this hangs? idk why
 
 
   });
@@ -48,8 +49,22 @@ puppeteer.launch({
 
   // await runTests(id);
   await browser.close();
-});
+}
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+async function getExtensionId(browser) {
+  // a hack to get id of current extension by loading options page and finding where it is displayed in texted
+  const page = await browser.newPage();
+  await page.goto('chrome://extensions');
+  const devToggle = await page.click('#toggle-dev-on');
+  const idHandle = await (await page.$('.extension-id'));
+  let id = await page.evaluate(body => body.textContent, idHandle);
+  await page.close()
+  id = id.trim();
+  return id;
+}
+
+runTests();
