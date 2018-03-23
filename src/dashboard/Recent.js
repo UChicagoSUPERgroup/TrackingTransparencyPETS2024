ee87911358c228bcd782b73d8f47564ece45909d
Claire Dolin <cdolin@uchicago.edu>
2018-03-23 18:42:54 -0500

    easy fixes putting graphs in a row and adding text

--- src/dashboard/Recent.js
+++ src/dashboard/Recent.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 // import { LinkContainer } from 'react-router-bootstrap';
 import tt from '../helpers';
+import {Grid, Row, Col} from 'react-bootstrap';
+
 
 import PagesTimeChart from './PagesTimeChart';
 import PagesTimeScatterplot from './PagesTimeScatterplot';
@@ -27,18 +29,34 @@ export default class RecentPage extends React.Component {
       });
     });
   }
-  
+
   render() {
     const {timestamps} = this.state;
     return(
       <div>
         <h1>Recent Activity</h1>
-
         <Route exact path={this.props.match.url} render={() => (
           <div>
-            {timestamps && <PagesTimeChart timestamps={timestamps}/>}
-            <br/>
-            {timestamps && <PagesTimeScatterplot timestamps={timestamps}/>}
+            <Grid>
+              <Row>
+              <p>
+              On this page you can learn about when you've been tracked recently.
+              On the left, the histogram shows how many pages you've visited at different times.
+              You can toggle between time of day, day of week, and day of month filters.
+              On the right, the scatter plot shows when you visit the most pages.
+              The axes show the time and day of week of your page visits.
+              The size of a circle corresponds to the number of pages visited.
+              </p>
+              </Row>
+              <Row>
+                <Col md={8} mdPush={4}>
+                  {timestamps && <PagesTimeScatterplot timestamps={timestamps}/>}
+                </Col>
+                <Col md={4} mdPull={8}>
+                  {timestamps && <PagesTimeChart timestamps={timestamps}/>}
+                </Col>
+              </Row>
+            </Grid>
           </div>
         )}/>
 
