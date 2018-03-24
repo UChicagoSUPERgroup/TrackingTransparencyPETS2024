c13858851f37370eac2447e295a830792e789319
Claire Dolin <cdolin@uchicago.edu>
2018-03-24 10:48:29 -0500

    time label

--- src/dashboard/PagesTimeChart.js
+++ src/dashboard/PagesTimeChart.js
@@ -16,9 +16,18 @@ import ToggleButton from 'react-bootstrap/lib/ToggleButton';
 
 import tt from '../helpers';
 
-function timeLabel(v) {
+function dateLabel(v) {
   if (v == Math.floor(v))
     return v.toString();
+  return "";
+}
+
+function timeLabel(v) {
+  let times = ["12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM",
+               "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM",
+               "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM"];
+  if (v == Math.floor(v))
+    return times[v % 24];
   return "";
 }
 
@@ -85,7 +94,7 @@ export default class PagesTimeChart extends React.Component {
     case 'month-day':
       grouped = _.groupBy(times, t => t.getDate());
       xTitle = 'Day of Month';
-      labelFunc = timeLabel;
+      labelFunc = dateLabel;
       break;
 
     case 'hour':
@@ -108,7 +117,7 @@ export default class PagesTimeChart extends React.Component {
     return (
       <div>
         <XYPlot
-          width={350}
+          width={400}
           height={300}
           margin={{left: 40, right: 40, top: 10, bottom: 40}}>
           <HorizontalGridLines />
