8a8db4222b0fd8835d5efd6d97b91685edea1ad8
Miranda Wei <weim@uchicago.edu>
2018-03-30 14:57:52 -0500

    add some text to lightbeam tab

--- src/dashboard/LightbeamWrapper.js
+++ src/dashboard/LightbeamWrapper.js
@@ -26,7 +26,14 @@ export default class LightbeamWrapper extends React.Component {
     return (
       <div>
         <h1>Lightbeam</h1>
-        <p>This is a visualization of how different trackers connect to each other.</p>
+        <p>Lightbeam is an add-on that enables you to see connections between the first and third party sites you interact with on the Web.</p>
+        <p>The web below is interactive: click and drag on icons to try to untangle the web!</p>
+        <p>A circle icon represents a site that you have visted, and a triangle icon represents a third party that has seen you online. </p>
+        <ul>
+          <li>A circle icon with many connections is a site you have visited that has lots of third party sites active on it.</li>
+          <li>A triangle icon with many connections is a third party site that has seen you on many different sites online.</li>
+        </ul>
+        <p>Learn more at <a href="https://addons.mozilla.org/en-US/firefox/addon/lightbeam/">Mozilla</a>.</p>
         <IFrame src={browser.runtime.getURL('lightbeam/index.html')} />
       </div>
     )
