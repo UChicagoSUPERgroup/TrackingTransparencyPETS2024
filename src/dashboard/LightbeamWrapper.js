3e6cedc31ce4c8e63c8b1c77b904b07a4b78733a
Miranda Wei <weim@uchicago.edu>
2018-04-18 11:48:48 -0500

    remove mozilla, rename as 'your tracker network'

--- src/dashboard/LightbeamWrapper.js
+++ src/dashboard/LightbeamWrapper.js
@@ -25,16 +25,14 @@ export default class LightbeamWrapper extends React.Component {
   render() {
     return (
       <div>
-        <h1>Lightbeam</h1>
-        <p>Lightbeam is an add-on that enables you to see connections between the first and third party sites you interact with on the Web.</p>
+        <h1>Your Tracker Network</h1>
+        <p>Your Tracker Network is a visualization of the connections between the first and third party sites you have interacted with on the Web.</p>
         <p>The web below is interactive: click and drag on icons to try to untangle the web!</p>
         <p>A circle icon represents a site that you have visted, and a triangle icon represents a third party that has seen you online. </p>
         <ul>
           <li>A circle icon with many connections is a site you have visited that has lots of third party sites active on it.</li>
           <li>A triangle icon with many connections is a third party site that has seen you on many different sites online.</li>
         </ul>
-        <p>Learn more at <a href="https://addons.mozilla.org/en-US/firefox/addon/lightbeam/">Mozilla</a>.</p>
-        <IFrame src={browser.runtime.getURL('lightbeam/index.html')} />
       </div>
     )
   }
