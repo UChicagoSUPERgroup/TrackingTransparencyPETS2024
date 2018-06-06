1637578bc2cb49acc03588189afd7d0378fbd87a
Ben Weinshel <weinshel@uchicago.edu>
2018-06-06 18:11:41 +0200

    reword a bit on about tab

--- src/dashboard/About.js
+++ src/dashboard/About.js
@@ -15,10 +15,9 @@ export class AboutPage extends React.Component {
   }
 
   render() {
-    const {numTrackers, numInferences, numPages} = this.state;
     return (
       <div>
-        <h1>About</h1>
+        <h1>About Tracking Transparency</h1>
         <p>
         Tracking is ubiquitous…
         </p>
@@ -32,14 +31,13 @@ export class AboutPage extends React.Component {
         <p> insert: how we make inferences</p>
         <p> insert: limits of our approach</p>
         <p> insert: how it compares to other extensions- could be different approach</p>
-        <h2>Your privacy</h2>
+        <h2>Your Privacy</h2>
         <p>To show you the data that other companies could gather about your browsing behavior, our
-        extension also needs to log your behavior and page visits. All of your data is stored in a
-        local database on your device and so nobody, not even our team of researchers, has access to
-        your information. Your data stays on your computer and is never sent to another server.</p>
-        <h2>Who we are</h2>
-        <p>The Tracking Transparency extension was built by a team within the <a href="https://super.cs.uchicago.edu" target="_blank" rel="noopener noreferrer">University of Chicago SUPERgroup</a>. It was developed by Ben Weinshel, Claire Dolin, Shawn Shan, Euirim Choi, and Aaron Goldman. The project is advised by Prof. Blase Ur at the University of Chicago, Prof. Michelle L. Mazurek at the University of Maryland, and Lorrie Faith Cranor at Carnegie Mellon University.</p>
-        <p>Our source code is available on <a href="https://github.com/UChicagoSUPERgroup/trackingtransparency" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
+        extension logs your behavior and page visits in a local database on your computer. The extension also sends a limited number of anonymized statistics so we can understand how people are using the extension. We send information about ???, ???, and ???. The specific websites you visit and your browsing history never leave your computer and are not shared in any way. The statistics collected will only be accessed by the University of Chicago research team. We may publish aggregate statistics and findings from the reported data, but will never sell or share your data. (TODO confirm that we can actually say we will *never* share)</p>
+        <h2>Who We Are</h2>
+        <p>The Tracking Transparency extension was built by a research team at the <a href="https://super.cs.uchicago.edu" target="_blank" rel="noopener noreferrer">University of Chicago SUPERgroup</a>. [Insert research team]. The project is advised by Blase Ur at the University of Chicago, Michelle L. Mazurek at the University of Maryland, and Lorrie Faith Cranor at Carnegie Mellon University.</p>
+        <p>Should you have any questions about the plugin or our associated research, you may email the research team at <a href="mailto:trackingtransparency@super.cs.uchicago.edu">trackingtransparency@super.cs.uchicago.edu</a>.</p>
+        <p>Our extension is open source, and the code is available under a free license at <a href="https://github.com/UChicagoSUPERgroup/trackingtransparency" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
       </div>
     )
   }
