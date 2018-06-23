b212d2ac96ff0b5e85245a675ee83341cd1afe7e
Ben Weinshel <weinshel@uchicago.edu>
2018-06-23 12:38:19 -0500

    dev not debug

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('debug'),
+      'process.env.NODE_ENV': JSON.stringify('dev'),
     })
   ]
 });
