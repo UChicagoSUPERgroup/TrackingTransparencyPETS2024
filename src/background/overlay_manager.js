e3b60a1ae0bb9665cf937ecc051d6edc354474bc
Ben Weinshel <weinshel@uchicago.edu>
2018-07-07 16:54:32 -0500

    fix grammar for only 1 tracker

--- src/background/overlay_manager.js
+++ src/background/overlay_manager.js
@@ -2,15 +2,18 @@ function generateInnerHTML(tabData) {
   let innerHTML;
   let trackerWords = '', inferWords = '';
 
-  if (tabData.trackers && tabData.trackers.length === 0) {
-    trackerWords = '<p>There are no trackers on this page!</p>'
-  }
-
-  if (tabData.trackers && tabData.trackers.length > 0) {
-    trackerWords = `<p><strong>${tabData.trackers[0]}</strong> and 
-    <span id="num-trackers">${(tabData.trackers.length - 1)}</span>
-     others are tracking you on this page.</p>`
-  }
+  if (tabData.trackers) {
+    let len = tabData.trackers.length
+    if (len <= 0) {
+      trackerWords = '<p>There are no trackers on this page!</p>'
+    } else if (len === 1) {
+      trackerWords = `<p><strong>${tabData.trackers[0]}</strong> is tracking you on this page.</p>`
+    } else {
+      trackerWords = `<p><strong>${tabData.trackers[0]}</strong> and 
+        <span id="num-trackers">${(tabData.trackers.length - 1)}</span>
+        others are tracking you on this page.</p>`
+    }
+  } 
 
   if (tabData.inference) {
     inferWords = `<p>We think this page is about <strong>${tabData.inference}</strong>.</p>`
