3e3d7773cf8785d8f4a8b8bef4d1b85a07990a4f
mirandawei <mirandaaa.wei@gmail.com>
2018-07-13 13:19:13 +0545

    Turned intro page into popup and prettified text
    TODO: 
    - remove intro page from navbar and integrate with homepage (ensure only 
    appears once)

--- src/dashboard/Intro.js
+++ src/dashboard/Intro.js
@@ -1,41 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {Panel, Grid, Row, Col} from 'react-bootstrap';
+import {Panel, Grid, Row, Col, Modal, Button} from 'react-bootstrap';
 const millisecondsInDay = 86400000;
 
 
 const inferenceList = (data) => {
+  console.log(data)
   return (
     <div>
-      {data.map(function(dataValue) {
-        let key = dataValue["DISTINCT(inference)"]
-        return (<span key={key}>
-          <Link to={{pathname: '/inferences/' + key}}>
-             {key},&nbsp;
-          </Link>
-        </span>);
-      })}
-    </div>
-  )
+      {data.map(p => <span>
+          <strong> {p["DISTINCT(inference)"]} </strong>
+      </span>)
+      .reduce((prev, curr) => [prev, ', ', curr])        // comma-delimit
+      }
+    </div>);
 }
 
 const trackerList = (data) => {
   return (<div>
-      {data.map(p => <span key={p.id}>{p.tracker}</span>)     // get just tracker name
+      {data.map(p => <span key={p.id}>
+        <strong>{p.tracker}</strong>
+      </span>)     // get just tracker name
            .reduce((prev, curr) => [prev, ', ', curr])        // comma-delimit
       }
     </div>);
   }
 
-
 export class Intro extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
 
-    }
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+    this.state = {
+      show: true
+    };
     //this.logClick = this.logClick.bind(this);
     //this.logLoad = this.logLoad.bind(this);
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
   }
 
   async getData() {
@@ -65,13 +75,38 @@ export class Intro extends React.Component {
     const {numTrackers, numInferences, numPages, recentInferences, topTrackers} = this.state;
     return (
       <div>
-        <h1> Welcome to Tracking Transparency!</h1>
-        <p> When you browse the Internet, you will encounter trackers online that track your browsing activity. In the last week, you visited <strong>{numPages} pages</strong> and encountered <strong>{numTrackers} trackers</strong>. </p>
-        <p> The trackers that you have encountered most frequently are {topTrackers ? trackerList(topTrackers) : ""}.</p>
-        <p> These companies could have inferred your interest in <strong>{numInferences} topics</strong>, like {recentInferences ? inferenceList(recentInferences) : ""} </p>
-        <p>Continue to the Tracking Transparency homepage to learn more about the trackers you have encountered, what they might have learned about you, and more.</p>
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Title closebutton="true"></Modal.Title>
+
+          <Modal.Body>
+            <h2>Welcome to Tracking Transparency!</h2>
+            <p> When you browse the Internet, third-party trackers can see your browsing activity and sell this information to advertising companies. We hope this extension will help you understand who is tracking you and what they could have learned.</p>
+            <p> In the last week, you visited <strong>{numPages} pages</strong> and encountered <strong>{numTrackers} trackers</strong>.</p>
+            <hr />
+            <h4> Your top 5 trackers: </h4>
+            <p>{topTrackers ? trackerList(topTrackers) : ""}</p>
+            <hr />
+            <h4> Your top 5 inferred interests: </h4>
+            <p> {recentInferences ? inferenceList(recentInferences) : ""} </p>
+            <hr />
+            <p>Continue to the homepage to learn more about the trackers you have encountered, what they might have learned about you, and more.</p>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button onClick={this.handleClose}>Continue</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
-    )
+
+      // <div>
+      //   <h1> Welcome to Tracking Transparency!</h1>
+      //   <p> When you browse the Internet, third-party trackers can see your browsing activity and sell this information to advertising companies. We hope this extension will help you understand who is tracking you and what they could have learned.</p>
+      //   <p> In the last week, you visited <strong>{numPages} pages</strong> and encountered <strong>{numTrackers} trackers</strong>. </p>
+      //   <p> The trackers that you have encountered most frequently are {topTrackers ? trackerList(topTrackers) : ""}.</p>
+      //   <p> These companies could have inferred your interest in <strong>{numInferences} topics</strong>, like {recentInferences ? inferenceList(recentInferences) : ""} </p>
+      //   <p>Continue to the Tracking Transparency homepage to learn more about the trackers you have encountered, what they might have learned about you, and more.</p>
+      // </div>
+    );
   }
 }
 
