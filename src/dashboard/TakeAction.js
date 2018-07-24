c4a3b0c8ac20c5679d26df81200daa230a7af3a4
mirandawei <mirandaaa.wei@gmail.com>
2018-07-24 12:46:42 +0200

    Continue style transition
    - change to checkboxes
    - remove vague topics, include specific actions

--- src/dashboard/TakeAction.js
+++ src/dashboard/TakeAction.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-import Heading from '@instructure/ui-elements/lib/components/Heading'
-import Text from '@instructure/ui-elements/lib/components/Text'
-import ToggleDetails from '@instructure/ui-toggle-details/lib/components/ToggleDetails'
-import ToggleGroup from '@instructure/ui-toggle-details/lib/components/ToggleGroup'
-import View from '@instructure/ui-layout/lib/components/View'
+import Heading from '@instructure/ui-elements/lib/components/Heading';
+import Text from '@instructure/ui-elements/lib/components/Text';
+import ToggleDetails from '@instructure/ui-toggle-details/lib/components/ToggleDetails';
+import ToggleGroup from '@instructure/ui-toggle-details/lib/components/ToggleGroup';
+import View from '@instructure/ui-layout/lib/components/View';
+import Checkbox from '@instructure/ui-forms/lib/components/Checkbox';
 
 import logging from './dashboardLogging';
 
@@ -24,78 +25,54 @@ export class TakeActionPage extends React.Component {
   render() {
     const {numTrackers, numInferences, numPages} = this.state;
     return (
-      <Text>
-        <Heading level='h1' margin='0 0 medium'>Settings</Heading>
+      <div>
+        <div>
+          <Heading level='h1'>Take Action</Heading>
+          <Text>
+            <p>We hope that you have learned something about online tracking by using our extension.</p>
+          </Text>
+          <Text>
+            <p>Although our extension does <em>not</em> change the operation of your browser, you can take action regarding online tracking. On this page, we list a number of options you could take. Please let us know if you plan to take any of these actions (or already do), as it will help us understand the impact of our extension. We may add your selections as features in the future.</p>
+          </Text>
+        </div>
 
-        <Heading level='h2'>Take Action</Heading>
-        <Text>
-          <p>We hope that you have learned something about online tracking by using our extension.</p>
-          <p>Indicate your preferences here about topics related to online tracking and consumer transparency. Your selections here will <strong>not</strong> change the operation of your browser, but will help us continue our work. We may add your selections as features in the future.</p>
-        </Text>
+        <div>
+          <Heading level='h2' border="top">Installing Tracker Blockers</Heading>
+          <Text>
+            <p>Several browser extensions serve as tracker blockers. These extensions prevent advertisers and other third-party services from tracking where and what you browse. Examples include <a href='https://www.eff.org/privacybadger' target='_blank' rel='noopener noreferrer'>Privacy Badger</a> and <a href='https://www.ghostery.com/' target='_blank' rel='noopener noreferrer'>Ghostery</a>. Some ad-blocking services may have this feature as well.</p>
+          </Text>
+          <Checkbox label="I would use a tracker blocker." value="medium"/>
+          <br />
+        </div>
 
-        <ToggleGroup
-          transition={false}
-          toggleLabel="This is the toggle button label for screenreaders"
-          summary="Trackers"
-        >
-          <View display="block" padding="small">I want no tracking, even if I won't see any relevant advertisements</View>
-          <View display="block" padding="small">I'm okay with some tracking, if it means that I will see advertisements relevant to me</View>
-          <View display="block" padding="small">I'm okay with all tracking because I want to see advertisements relevant to me</View>
-        </ToggleGroup>
-
-
-        <ToggleGroup
-          transition={false}
-          toggleLabel="This is the toggle button label for screenreaders"
-          summary="Inferencing"
-        >
-          <View display="block" padding="small">I would opt-out of all inferencing</View>
-          <View display="block" padding="small">I would only want non-sensitive inferencing</View>
-          <View display="block" padding="small">Any and all inferencing is fine</View>
-        </ToggleGroup>
-
-        <ToggleGroup
-          transition={false}
-          toggleLabel="This is the toggle button label for screenreaders"
-          summary="Installing Tracker Blockers"
-          >
-            <p>Several browser extensions serve as tracker blockers. These extensions prevent advertisers and other third-party services from tracking where and what you browse. We recommend <a href='https://www.eff.org/privacybadger' target='_blank' rel='noopener noreferrer'>Privacy Badger</a> or <a href='https://www.ghostery.com/' target='_blank' rel='noopener noreferrer'>Ghostery</a>. Some ad-blocking services may have this feature as well.</p>
-          <View display="block" padding="small">I would use a tracker blocker.</View>
-          <View display="block" padding="small">I would not use a tracker blocker.</View>
-        </ToggleGroup>
-
-        <ToggleGroup
-          transition={false}
-          toggleLabel="This is the toggle button label for screenreaders"
-          summary="Cookies"
-          >
+        <div>
+          <Heading level='h2' border="top">Cookies</Heading>
+          <Text>
             <p>Cookies are pieces of data maintained by the web browser to store information about the way you browse. They may be used to track you as well. Each advertiser gives you the option to disable cookie tracking. You can learn more  <a href='http://optout.aboutads.info/?c=2#!/' target='_blank' rel='noopener noreferrer'>here</a>.</p>
-          <View display="block" padding="small">I would block cookies.</View>
-          <View display="block" padding="small">I would not block cookies.</View>
-        </ToggleGroup>
+          </Text>
+          <Checkbox label="I would block cookies." value="medium"/>
+          <br />
+        </div>
 
-        <ToggleGroup
-          transition={false}
-          toggleLabel="This is the toggle button label for screenreaders"
-          summary="Do Not Track (DNT) setting"
-          >
+        <div>
+          <Heading level='h2' border="top">Do Not Track (DNT) Setting</Heading>
+          <Text>
             <p>Do Not Track (DNT) is a browser setting that sends the request <q> don&#39;t want to be tracked</q> with every website you visit. it’s like a setting in your browser preferences. However, few companies respect this request.</p>
-          <View display="block" padding="small">I would enable DNT.</View>
-          <View display="block" padding="small">I would not enable DNT.</View>
-        </ToggleGroup>
+          </Text>
+          <Checkbox label="I would enable the DNT setting." value="medium"/>
+          <br />
+        </div>
 
-
-        <ToggleGroup
-          transition={false}
-          toggleLabel="This is the toggle button label for screenreaders"
-          summary="Data Broker Profiles"
-          >
+        <div>
+          <Heading level='h2' border="top">Data Broker Profiles</Heading>
+          <Text>
             <p>Data brokers are third-parties that collect data about you when you use your browser. To prevent these third-parties from collecting information about you and remove any profiles they may already have of you, you can follow <a href='https://www.the-parallax.com/2016/04/07/how-to-clean-up-or-delete-data-brokers-profiles-of-you/' target='_blank' rel='noopener noreferrer'>this article</a>.</p>
-          <View display="block" padding="small">I would adjust the profile that data brokers have about me.</View>
-          <View display="block" padding="small">I would not adjust the profile that data brokers have about me.</View>
-        </ToggleGroup>
+          </Text>
+          <Checkbox label="I would adjust the profile that data brokers have about me." value="medium"/>
+          <br />
+        </div>
 
-      </Text>
+      </div>
 
     )
   }
