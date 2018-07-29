d177a6f89685b7b885beaa40b9b3b422db8a91c9
Ben Weinshel <weinshel@uchicago.edu>
2018-07-29 15:22:16 -0500

    small text changes

--- src/dashboard/TakeAction.js
+++ src/dashboard/TakeAction.js
@@ -3,12 +3,11 @@ import { Link } from 'react-router-dom';
 
 import Heading from '@instructure/ui-elements/lib/components/Heading';
 import Text from '@instructure/ui-elements/lib/components/Text';
-import ToggleDetails from '@instructure/ui-toggle-details/lib/components/ToggleDetails';
-import ToggleGroup from '@instructure/ui-toggle-details/lib/components/ToggleGroup';
-import View from '@instructure/ui-layout/lib/components/View';
 import Checkbox from '@instructure/ui-forms/lib/components/Checkbox';
 
 import logging from './dashboardLogging';
+
+const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1
 
 export class TakeActionPage extends React.Component {
   constructor(props) {
@@ -32,39 +31,40 @@ export class TakeActionPage extends React.Component {
             <p>We hope that you have learned something about online tracking by using our extension.</p>
           </Text>
           <Text>
-            <p>Although our extension does <em>not</em> change the operation of your browser, you can take action regarding online tracking. On this page, we list a number of options you could take. Please let us know if you plan to take any of these actions (or already do), as it will help us understand the impact of our extension. We may add your selections as features in the future.</p>
+            <p>Although our extension does <em>not</em> change the operation of your browser, you can take action regarding online tracking using third-party tools and exercising choices that advertisers provide. On this page, we list a number of options you could take. Please let us know if you plan to take any of these actions (or already do), as it will help us understand the impact of our extension. We may add your selections as features in the future.</p>
           </Text>
         </div>
 
         <div>
-          <Heading level='h2' border="top">Installing Tracker Blockers</Heading>
+          <Heading level='h2' border='top' margin='medium none none none'>Block trackers</Heading>
           <Text>
-            <p>Several browser extensions serve as tracker blockers. These extensions prevent advertisers and other third-party services from tracking where and what you browse. Examples include <a href='https://www.eff.org/privacybadger' target='_blank' rel='noopener noreferrer'>Privacy Badger</a> and <a href='https://www.ghostery.com/' target='_blank' rel='noopener noreferrer'>Ghostery</a>. Some ad-blocking services may have this feature as well.</p>
+            <p>Several browser extensions serve as tracker blockers. These extensions prevent advertisers and other third-party services from tracking where and what you browse. Examples include <a href='https://www.eff.org/privacybadger' target='_blank' rel='noopener noreferrer'>Privacy Badger</a> and the <a href='https://duckduckgo.com/app' target='_blank' rel='noopener noreferrer'>DuckDuckGo extension</a>. Some ad-blocking services may have this feature as well.</p>
+            {isFirefox && <p>In Firefox, you can enable the built-in tracking protection feature. See <a href='https://support.mozilla.org/en-US/kb/tracking-protection' target='_blank' rel='noopener noreferrer'>here</a> for directions.</p>}
           </Text>
           <Checkbox label="I would use a tracker blocker." value="medium"/>
           <br />
         </div>
 
         <div>
-          <Heading level='h2' border="top">Cookies</Heading>
+          <Heading level='h2' border='top' margin='medium none none none'>Opt-out cookies</Heading>
           <Text>
-            <p>Cookies are pieces of data maintained by the web browser to store information about the way you browse. They may be used to track you as well. Each advertiser gives you the option to disable cookie tracking. You can learn more  <a href='http://optout.aboutads.info/?c=2#!/' target='_blank' rel='noopener noreferrer'>here</a>.</p>
+            <p>Cookies are pieces of data maintained by the web browser to store information about the way you browse. They may be used to track you as well. Many advertisers give you the option to "opt out" of cookie tracking. You can learn more and opt-out <a href='http://optout.aboutads.info/' target='_blank' rel='noopener noreferrer'>here</a>.</p>
           </Text>
-          <Checkbox label="I would block cookies." value="medium"/>
+          <Checkbox label="I would opt-out of tracking cookies." value="medium"/>
           <br />
         </div>
 
         <div>
-          <Heading level='h2' border="top">Do Not Track (DNT) Setting</Heading>
+          <Heading level='h2' border='top' margin='medium none none none'>Do Not Track (DNT) setting</Heading>
           <Text>
-            <p>Do Not Track (DNT) is a browser setting that sends the request <q> don&#39;t want to be tracked</q> with every website you visit. it’s like a setting in your browser preferences. However, few companies respect this request.</p>
+            <p>Do Not Track (DNT) is a browser setting that sends the request <q>I don&#39;t want to be tracked</q> with every website you visit. It’s a setting in your browser preferences. However, few companies respect this request. You can learn more about DNT and how to set it <a href='https://allaboutdnt.com/' target='_blank' rel='noopener noreferrer'>here</a>.</p>
           </Text>
           <Checkbox label="I would enable the DNT setting." value="medium"/>
           <br />
         </div>
 
         <div>
-          <Heading level='h2' border="top">Data Broker Profiles</Heading>
+          <Heading level='h2' border='top' margin='medium none none none'>Data broker profiles</Heading>
           <Text>
             <p>Data brokers are third-parties that collect data about you when you use your browser. To prevent these third-parties from collecting information about you and remove any profiles they may already have of you, you can follow <a href='https://www.the-parallax.com/2016/04/07/how-to-clean-up-or-delete-data-brokers-profiles-of-you/' target='_blank' rel='noopener noreferrer'>this article</a>.</p>
           </Text>
