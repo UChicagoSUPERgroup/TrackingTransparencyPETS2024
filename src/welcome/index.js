9431172e01fb96dbfa4b6ee861194f8e7b36fcb3
mirandawei <mirandaaa.wei@gmail.com>
2018-09-26 18:01:39 -0500

    added screenshot for extension in toolbar

--- src/welcome/index.js
+++ src/welcome/index.js
@@ -40,7 +40,10 @@ class WelcomePage extends React.Component {
           {/*<Heading margin='large 0 medium 0'>Welcome to Tracking Transparency!</Heading>*/}
           <Text>
             <p>Thank you for agreeing to participate in our research study. To activate our browser extension, please copy the activation code that is displayed at the end of the survey.</p>
-            <p>This browser extension is a software tool that visualizes aspects of your web browsing.</p>
+            <p>This browser extension is a software tool that visualizes aspects of your web browsing. In order to access the extension, click on the icon in the corner of the upper right of your browser window (Chrome and Firefox). </p>
+          <Text>
+          <img src="/icons/extension-toolbar.png" width='500px'/>
+          </Text>
             <p>As we described in the consent form, the browser extension will store on your own computer data about your web browsing from while the extension is installed. This is necessary so that the extension can visualize your web browsing for you. This detailed data will not leave your computer and will not be shared with the researchers. The software will, however, collect for the researchers non-identifiable metadata, including broad descriptions of the topics of webpages you visit (e.g., “entertainment” or “computer hardware”) as well as how many different websites you visit, but not which particular websites you visit. This metadata will also include broad, non-identifiable usage statistics about your web browsing (e.g., the times of day you browse) and how you interact with the browser extension. Only this non-identifiable data and your survey responses will be collected by the researchers. At the end of the study, we will provide detailed instructions for uninstalling this browser extension.</p>
           </Text>
         </div>
