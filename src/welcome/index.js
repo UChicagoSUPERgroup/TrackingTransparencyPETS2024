7f63cb96bc0117c18ac6b6b1801dd438183ba39a
mirandawei <mirandaaa.wei@gmail.com>
2018-09-26 19:02:59 -0500

    add activation code input box and continue button on welcome

--- src/welcome/index.js
+++ src/welcome/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom'
 import theme from '@instructure/ui-themes/lib/canvas'
 import Heading from '@instructure/ui-elements/lib/components/Heading'
 import Text from '@instructure/ui-elements/lib/components/Text'
+import TextInput from '@instructure/ui-forms/lib/components/TextInput'
+import Button from '@instructure/ui-buttons/lib/components/Button'
 
 import logging from '../dashboard/dashboardLogging'
 import { themeOverrides } from '../colors'
@@ -32,20 +34,39 @@ class WelcomePage extends React.Component {
   async componentDidMount () {
   }
 
+  onMTurkCodeInput (event) {
+    browser.storage.local.set({ mturkcode: event.target.value})
+  }
+
   render () {
     return (
       <div>
         <div className='container' style={styles.container}>
-          <img src="/icons/super.svg" height='150px'/>
+          <img src="/icons/super.svg" height='150px' />
           {/*<Heading margin='large 0 medium 0'>Welcome to Tracking Transparency!</Heading>*/}
+
           <Text>
-            <p>Thank you for agreeing to participate in our research study. To activate our browser extension, please copy the activation code that is displayed at the end of the survey.</p>
-            <p>This browser extension is a software tool that visualizes aspects of your web browsing. In order to access the extension, click on the icon in the corner of the upper right of your browser window (Chrome and Firefox). </p>
+            <p>This browser extension is a software tool that visualizes aspects of your web browsing. To access the extension, click on the icon in the corner of the upper right of your browser window. </p>
+          </Text>
+          <img src="/icons/extension-toolbar.png" width='700px' style={{border:"2px solid black"}} />
           <Text>
-          <img src="/icons/extension-toolbar.png" width='500px'/>
+            <p>The extension icon will appear in the upper right corner for Chrome as well as Firefox users. </p>
           </Text>
-            <p>As we described in the consent form, the browser extension will store on your own computer data about your web browsing from while the extension is installed. This is necessary so that the extension can visualize your web browsing for you. This detailed data will not leave your computer and will not be shared with the researchers. The software will, however, collect for the researchers non-identifiable metadata, including broad descriptions of the topics of webpages you visit (e.g., “entertainment” or “computer hardware”) as well as how many different websites you visit, but not which particular websites you visit. This metadata will also include broad, non-identifiable usage statistics about your web browsing (e.g., the times of day you browse) and how you interact with the browser extension. Only this non-identifiable data and your survey responses will be collected by the researchers. At the end of the study, we will provide detailed instructions for uninstalling this browser extension.</p>
+          <Text fontStyle="italic">
+            <hr/>
+            <p>As described in the consent form, this browser extension will store data about your web browsing on your own computer. This detailed data will not leave your computer and will not be shared with the researchers. The extension will send non-identifiable metadata and your survey responses to the researchers. At the end of the study, we will provide detailed instructions for uninstalling this browser extension.</p>
+            <hr/>
           </Text>
+          <Text>
+            <p>To activate our browser extension, please enter the activation code that is displayed at the end of the survey:</p>
+          </Text>
+          <TextInput label= "" placeholder="activation code from MTurk" onChange={this.onMTurkCodeInput}/>
+          <Text>
+            <p>By clicking continue, you indicate that you accept the consent form and will be taken to the extension homepage.</p>
+          </Text>
+          <Button variant="primary" margin="0 x-small 0 0">
+              Continue
+            </Button>
         </div>
 
       </div>
