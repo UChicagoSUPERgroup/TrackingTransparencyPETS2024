2d1da6d5f859cbade1d5a25e52b2951c69c8c562
Ben Weinshel <weinshel@uchicago.edu>
2018-09-27 22:14:14 -0500

    get acceptable prod builds

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,27 +1,27 @@
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
 
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map',
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        uglifyOptions: {
-          mangle: false,
-          keep_classnames: true,
-          keep_fnames: true,
-          output: {
-            beautify: true,
-            comments: true
-          }
-        }
-      })
+      // new UglifyJsPlugin({
+      //   cache: true,
+      //   parallel: true,
+      //   uglifyOptions: {
+      //     uglifyOptions: {
+      //       mangle: false,
+      //       output: {
+      //         beautify: true,
+      //         comments: true,
+      //         semicolons: false
+      //       }
+      //     }
+      //   }
+      // })
     ]
   }
 })
