bf5c60c04ab46e9fc553743a346c6fe571bed3ac
mirandawei <mirandaaa.wei@gmail.com>
2018-10-24 16:47:50 -0500

    clean up about page for all conditions - removed references to "personalized examples" in case user is in condition without those

--- src/dashboard/Info.js
+++ src/dashboard/Info.js
@@ -36,24 +36,26 @@ export default class InfoPage extends React.Component {
               <Text>
                 <p>
             With this extension, we hope to bring you more transparency about the world of online tracking, analytics, and advertising. We track the trackers by looking for trackers on all of the pages you visit while the extension is running. The extension keeps track of
-            where and when you encountered these trackers. All of this information is used to show you personalized examples of how you interact with trackers during your normal browsing habits.
+            where and when you encountered these trackers. All of this information is used to show you examples of how one could interact with trackers during their normal browsing habits.
                 </p>
                 <p>
             Tracking Transparency runs in the background while you browse online to collect information about these online trackers.
                 </p>
-                <p>
+                {/* <p>
           We collect information about:
+                </p>
                   <ul>
                     <li>Which websites you visit</li>
                     <li>Which trackers are on these websites</li>
                   </ul>
+                  <p>
           Using this information, we show you:
+                  </p>
                   <ul>
                     <li>Which <Link href='#/trackers'>trackers</Link> have tracked you </li>
                     <li>Which <Link href='#/interests'>interests</Link> may have been inferred about your browsing</li>
                     <li>When inferences have been made about you during your <Link href='#/activity'>recent activity</Link></li>
-                  </ul>
-                </p>
+                  </ul> */}
               </Text>
             </TTPanel>
           </GridCol>
@@ -65,10 +67,10 @@ export default class InfoPage extends React.Component {
               <Heading level='h2'>Your privacy</Heading>
               <Text>
                 <p>
-            The data that Tracking Transparency collects about you is securely stored in your local browser. Your data is never sent to another server, so not even the researchers and developers of Tracking Transparency have access to your data.
+            The data that Tracking Transparency collects about you is securely stored in your local browser. Your personal data is never sent to another server, so not even the researchers and developers of Tracking Transparency have access to any personally-identifiable information.
                 </p>
                 <p>
-            To show you the data that other companies could gather about your browsing behavior, our extension logs your behavior and page visits in a local database on your computer. The extension also sends a small number of anonymized statistics so we can understand how people are using the extension. We send information about ???, ???, and ???. The specific websites you visit and your browsing history never leave your computer and are not shared in any way. The statistics collected will only be accessed by the University of Chicago research team. We may publish aggregate statistics and findings from the reported data, but will never sell your data.
+            To show you the data that other companies could gather about your browsing behavior, our extension logs your behavior, page visits, and an inferred topic of your visited pages in a local database on your computer. The extension also sends a small number of anonymized statistics so we can understand how people are using the extension. The specific websites you visit and your browsing history never leave your computer and are not shared in any way. The statistics collected will only be accessed by the University of Chicago research team. We may publish aggregate statistics and findings from the reported data, but will never sell your data.
                 </p>
                 <p>
                   To learn more, please read our <a href='https://super.cs.uchicago.edu/trackingtransparency/privacy.html' target='_blank'>privacy policy</a>.
