4700a4606c101e6ab328dc34fc99b54f44b951fb
mirandawei <mirandaaa.wei@gmail.com>
2018-10-29 13:22:59 -0500

    update about page language

--- src/dashboard/Info.js
+++ src/dashboard/Info.js
@@ -35,27 +35,14 @@ export default class InfoPage extends React.Component {
               <Heading level='h2'>How the extension works</Heading>
               <Text>
                 <p>
-            With this extension, we hope to bring you more transparency about the world of online tracking, analytics, and advertising. We track the trackers by looking for trackers on all of the pages you visit while the extension is running. The extension keeps track of
-            where and when you encountered these trackers. All of this information is used to show you examples of how one could interact with trackers during their normal browsing habits.
+            With this extension, we hope to bring you more transparency about the world of online tracking, analytics, and advertising. Tracking Transparency runs in the background while you browse online to collect information about these online trackers.
                 </p>
                 <p>
-            Tracking Transparency runs in the background while you browse online to collect information about these online trackers.
+            We track the trackers by looking for data being sent to or from known trackers. We determine whether an entity is a tracker with Disconnect's open-source  <a href = "https://disconnect.me/trackerprotection/blocked" target='_blank' rel='noopener noreferrer'>list of known trackers</a>. Unlike other ad or tracker blockers you might use, this extension does <em>not</em> block ads or otherwise change any aspect of your normal browsing. Instead, while the extension is running, it simply observes where and when you may have encountered these trackers.
                 </p>
-                {/* <p>
-          We collect information about:
+                <p>
+            Tracking Transparency also makes guesses about what trackers could have learned about you. Many trackers operate by collecting information about you and your interests and selling this data to other entities. This extension has a built-in algorithm to guess the topic of a web page based on the content, and uses this information to help you understand what trackers could be learning about you.
                 </p>
-                  <ul>
-                    <li>Which websites you visit</li>
-                    <li>Which trackers are on these websites</li>
-                  </ul>
-                  <p>
-          Using this information, we show you:
-                  </p>
-                  <ul>
-                    <li>Which <Link href='#/trackers'>trackers</Link> have tracked you </li>
-                    <li>Which <Link href='#/interests'>interests</Link> may have been inferred about your browsing</li>
-                    <li>When inferences have been made about you during your <Link href='#/activity'>recent activity</Link></li>
-                  </ul> */}
               </Text>
             </TTPanel>
           </GridCol>
@@ -73,7 +60,7 @@ export default class InfoPage extends React.Component {
             To show you the data that other companies could gather about your browsing behavior, our extension logs your behavior, page visits, and an inferred topic of your visited pages in a local database on your computer. The extension also sends a small number of anonymized statistics so we can understand how people are using the extension. The specific websites you visit and your browsing history never leave your computer and are not shared in any way. The statistics collected will only be accessed by the University of Chicago research team. We may publish aggregate statistics and findings from the reported data, but will never sell your data.
                 </p>
                 <p>
-                  To learn more, please read our <a href='https://super.cs.uchicago.edu/trackingtransparency/privacy.html' target='_blank'>privacy policy</a>.
+                  To learn more, please read our <a href='https://super.cs.uchicago.edu/trackingtransparency/privacy.html' target='_blank' rel='noopener noreferrer'>privacy policy</a>.
                 </p>
               </Text>
             </TTPanel>
@@ -87,7 +74,7 @@ export default class InfoPage extends React.Component {
               <Text>
                 <p>The Tracking Transparency extension was built by a research team at the <Link href='https://super.cs.uchicago.edu' target='_blank' rel='noopener noreferrer'>University of Chicago SUPERgroup</Link>. The project is advised by Blase Ur at the University of Chicago, Michelle L. Mazurek at the University of Maryland, and Lorrie Faith Cranor at Carnegie Mellon University.</p>
                 <p>Should you have any questions about the plugin or our associated research, you may email the research team at <Link href='mailto:trackingtransparency@super.cs.uchicago.edu'>trackingtransparency@lists.uchicago.edu</Link>.</p>
-                <p>Our extension is open source, and the code is available under Link free license at <Link href='https://github.com/UChicagoSUPERgroup/trackingtransparency' target='_blank' rel='noopener noreferrer'>GitHub</Link>.</p>
+                <p>Our extension is open source, and the code is available under Link-free license at <Link href='https://github.com/UChicagoSUPERgroup/trackingtransparency' target='_blank' rel='noopener noreferrer'>GitHub</Link>.</p>
               </Text>
             </TTPanel>
           </GridCol>
