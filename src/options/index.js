13354c5e75b937f4d55efc58c3c0d2baedd84efd
Ben Weinshel <weinshel@uchicago.edu>
2018-11-03 20:00:05 -0500

    add options & info for overlay

--- src/options/index.js
+++ src/options/index.js
@@ -1,16 +1,79 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import FormFieldGroup from '@instructure/ui-forms/lib/components/FormFieldGroup'
+import Checkbox from '@instructure/ui-forms/lib/components/Checkbox'
+import RadioInputGroup from '@instructure/ui-forms/lib/components/RadioInputGroup'
+import RadioInput from '@instructure/ui-forms/lib/components/RadioInput'
+import Link from '@instructure/ui-elements/lib/components/Link'
+import Text from '@instructure/ui-elements/lib/components/Text'
 import theme from '@instructure/ui-themes/lib/canvas'
 
 import { themeOverrides } from '../colors'
 
-import {
-  SettingsPage
-} from '../dashboard/loadable'
+theme.use({ overrides: themeOverrides })
 
-theme.use({
-  overrides: themeOverrides
-})
+class Options extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      showOverlay: true
+    }
 
-ReactDOM.render(<SettingsPage />, document.getElementById('root'))
+    this.checkboxChangeHandler = this.checkboxChangeHandler.bind(this)
+    this.setOption = this.setOption.bind(this)
+    this.loadOptions = this.loadOptions.bind(this)
+  }
+
+  checkboxChangeHandler (e) {
+    let name = e.target.value
+    let state = e.target.checked
+    this.setOption(name, state)
+  }
+
+  async setOption (name, state) {
+    await this.setState({ [name]: state })
+    browser.storage.local.set({ options: this.state })
+  }
+
+  async loadOptions () {
+    const store = await browser.storage.local.get(['options', 'mturkcode'])
+    const options = store.options
+    const mturkcode = store.mturkcode
+    this.setState({
+      ...options,
+      id: mturkcode
+    })
+  }
+
+  async componentDidMount () {
+    this.loadOptions()
+  }
+
+  render () {
+    const id = this.state.id
+    return (
+      <div>
+        <FormFieldGroup
+          name='displayOptions'
+          description='Display options'
+          vAlign='top'
+          rowSpacing='small'
+        >
+          <Checkbox
+            value='showOverlay'
+            label='Show in-page overlay'
+            checked={this.state.showOverlay}
+            onChange={this.checkboxChangeHandler}
+            variant='toggle'
+            layout='inline'
+          />
+        </FormFieldGroup>
+        <Text>
+          <p><strong>User ID: {id}</strong></p>
+        </Text>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<Options />, document.getElementById('root'))
--- src/welcome/index.js
+++ src/welcome/index.js
@@ -62,6 +62,7 @@ class WelcomePage extends React.Component {
     }
     await saveID(id)
     this.setState({ id })
+    this.renderOverlayInfo = this.renderOverlayInfo.bind(this)
   }
 
   async onSave () {
@@ -78,6 +79,18 @@ class WelcomePage extends React.Component {
     await background.setExtEnabled(id, checked)
     const adblockers = await background.getAdblockers()
     this.setState({ adblockers })
+  }
+
+  renderOverlayInfo () {
+    const id = this.state.id || '6'
+    const cond = (id.split('-')[0])
+    if (cond === '4') { // ghostery condition
+      return (<div>
+        <p>{EXT.NAME} will load an overlay on each page with the current tracker as displayed below. If you wish to disable the overlay you may do so in your browser's extension options page.</p>
+        <img src='/icons/overlay.png' width='700px' style={{border: '1px solid black'}} />
+      </div>)
+    }
+    return null
   }
 
   // renderAdblockTable () {
@@ -137,6 +150,7 @@ class WelcomePage extends React.Component {
             <p>{EXT.NAME} is a software tool that visualizes aspects of your web browsing. To access this extension, click on the icon in the corner of the upper right of your browser window. </p>
             <img src='/icons/extension-toolbar.png' width="700px" style={{border: '1px solid black'}} />
             <p>The extension icon will appear in the upper right corner for Chrome as well as Firefox users. </p>
+            {this.renderOverlayInfo()}
           </Text>
           <Heading margin='large 0 medium 0' border='bottom'>Our study</Heading>
           <Text>
